1286729d140b8c3c35a5061bf7b34c601b1c27cd
Zhaocheng Du <35942558+ZhaochengD@users.noreply.github.com>
2019-10-26 10:44:30 -0400

    Create main.py

---
+++ K-Means/code/main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from K_means import *
+
+DATASET = "iris.csv"
+K_NUM   = 3
+EPOCH   = 100
+
+clt = K_means(DATASET, K_NUM, EPOCH)
+clt.train()
